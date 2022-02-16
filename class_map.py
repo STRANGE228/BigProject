ba3d61a9e8705a3b302e094ceb7d6441c2497ad8
STRANGE228 <77876859+STRANGE228@users.noreply.github.com>
2022-02-16 16:09:47 +0300

    Add files via upload

--- class_map.py
+++ class_map.py
@@ -1,10 +1,7 @@
 import pygame as pg
 from io import BytesIO
-
 import requests
 from PIL import Image
-
-from const import *
 
 
 def image_conv(bstring):
@@ -18,30 +15,50 @@ def image_conv(bstring):
 
 class Map(pg.sprite.Sprite):
     """
-
     """
     def __init__(self, *group):
         super(Map, self).__init__(*group)
         self.remake = True
+        self.need_search = True
         self.image = None
         self.rect = None
         self.pos = [0, 0]
-        self.pos_step = 0.001
-        self.zoom = 14
+        self.pos_step = 0.016 * 2.5
+        self.spn = 0.016
+        # self.zoom = 14
         self.mode = "sat"
+        self.pts = []
         self.fstring = 'Белая Холуница'
 
     def update(self, events):
         for event in events:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_PAGEUP:
-                    self.zoom = max(self.zoom - 1, 1)
+                    self.spn = max(self.spn * 2, 0.0005)
+                    # self.zoom = max(self.zoom - 1, 1)
                     self.remake = True
+                    self.pos_step = self.spn * 2.5
                 elif event.key == pg.K_PAGEDOWN:
-                    self.zoom = min(self.zoom + 1, 19)
+                    self.spn = min(self.spn / 2, 65.536)
+                    # self.zoom = min(self.zoom + 1, 19)
+                    self.remake = True
+                    self.pos_step = self.spn * 2.5
+                elif event.key == pg.K_UP:
+                    self.pos[1] = min(self.pos[1] + self.pos_step, 85)
+                    self.remake = True
+                elif event.key == pg.K_DOWN:
+                    self.pos[1] = max(self.pos[1] - self.pos_step, -85)
+                    self.remake = True
+                elif event.key == pg.K_LEFT:
+                    self.pos[0] = max(self.pos[0] - self.pos_step, -179)
+                    self.remake = True
+                elif event.key == pg.K_RIGHT:
+                    self.pos[0] = min(self.pos[0] + self.pos_step, 179)
                     self.remake = True
         if self.remake:
-            self.search()
+            if self.need_search:
+                self.search()
+                self.need_search = False
             self.get_map()
             self.remake = False
 
@@ -53,9 +70,11 @@ class Map(pg.sprite.Sprite):
         api_map = "http://static-maps.yandex.ru/1.x/"
         params = {
             "ll": f'{self.pos[0]},{self.pos[1]}',
-            # "spn": f'{self.spn},{self.spn}',
-            "z": self.zoom,
-            "l": self.mode
+            "spn": f'{self.spn},{self.spn}',
+            # "z": self.zoom,
+            "l": self.mode,
+            "size": "650,450",
+            "pt": "~".join([f'{elem[0]},{elem[1]},pm2ywl' for elem in self.pts])
         }
         request = requests.get(api_map, params=params)
         if request.status_code == 200:
@@ -75,5 +94,7 @@ class Map(pg.sprite.Sprite):
             result = request.json()
             self.pos = list(map(float, result["response"]["GeoObjectCollection"][
                 "featureMember"][0]["GeoObject"]["Point"]["pos"].split()))
+            if not self.pts:
+                self.pts.append(self.pos)
         else:
             print(request.status_code)
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import sys
-import pygame as pg
 from const import *
 from class_map import Map
 from class_input import InputStr
+from class_button import *
 
 pg.init()
 level0_sprites = pg.sprite.Group()
 level1_sprites = pg.sprite.Group()
 level2_sprites = pg.sprite.Group()
-# Добавляем классы
+
+names = {' схема   ': 'map',
+         'спутник': 'sat',
+         'гибрид ': 'sat,skl'}
+
 main_map = Map(level0_sprites)
-input1 = InputStr(10, 10, 100, 20, level1_sprites)
+input1 = InputStr(10, (HEIGHT - 150), 100, 20, level1_sprites)
+type_map = ButtonMap(10, (HEIGHT - 250), 100, 50, level1_sprites)
+search_map = ButtonSearch((WIDTH - 110), (HEIGHT - 250), 100, 50, level1_sprites)
+clear_map = ButtonClear(120, (HEIGHT - 50), 100, 50, level1_sprites)
+
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
+# pg.key.set_repeat(300, 100)
 FPS = 25
 
 work = True
@@ -31,10 +40,28 @@ while work:
     level1_sprites.draw(screen)
     level2_sprites.draw(screen)
 
+    if clear_map.clear:
+        main_map.pts = []
+        clear_map.clear = False
+        main_map.remake = True
+
+    if search_map.remake:
+        main_map.pts = []
+        search_map.remake = False
+        input1.text_out = input1.text
+        input1.text = ""
+
     if input1.text_out:
+        main_map.pts = []
         main_map.fstring = input1.text_out
+        main_map.need_search = True
         main_map.remake = True
         input1.text_out = ''
+
+    if type_map.remake:
+        main_map.mode = names[type_map.text]
+        type_map.remake = False
+        main_map.remake = True
 
     pg.display.flip()
     clock.tick(FPS)
